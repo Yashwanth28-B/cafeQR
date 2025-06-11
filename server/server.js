824d7380a202b61846a5ded8950ac7134c648914
Yashwanth B Kalyan <appuy8164@gmail.com>
2025-06-11 10:11:31 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use(
             "http://localhost:3000",
             "http://localhost:5173",
             "http://localhost:8081",
+            "http://localhost:8080",
           ],
     credentials: true,
   })
